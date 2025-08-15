839f4500e4f833d6c84d27bf83fe9585f03d27ff
ArtemSpr <artemspravnikov77@gmail.com>
2025-08-15 13:24:05 +0300

    Change function name and add file path

--- backend/Program.cs
+++ backend/Program.cs
@@ -2,13 +2,23 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("hellous/", HelloMethod);
+app.MapGet("hellous/", GetHello);
+app.MapGet("admin/", () => "We both know that u are not admin");
 
 app.Run();
 
-string HelloMethod()
+string GetHello()
 {
-    var message = System.IO.File.ReadAllText("./hello.txt");
+    var helloFolder = new DirectoryInfo(Directory.GetCurrentDirectory()); // "Empty string to use current directory"
 
+    var file = new FileInfo("./hello.txt");
+
+    var filePath = file.FullName;
+    var helloPath = Path.Combine(helloFolder.FullName, "hello.txt");
+    var helloPath2 = Path.Combine(Directory.GetCurrentDirectory(), "hello.txt");
+
+    Console.WriteLine($"Reading file: {filePath}");
+
+    var message = System.IO.File.ReadAllText(helloPath);
     return message;
 }
