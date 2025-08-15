ba7a78344a530bf13a6a00ac03183528c62b3f47
ArtemSpr <artemspravnikov77@gmail.com>
2025-08-15 12:24:21 +0300

    Add HelloMethod function

--- backend/Program.cs
+++ backend/Program.cs
@@ -2,5 +2,12 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("hellous/", HelloMethod);
 
 app.Run();
+
+string HelloMethod()
+{
+    var message = "MoIkKa!";
+    return message;
+}
